2cc9f96abeddd302ab69c7c74c6e54d8279f9ec6
Marcia <marciaadler718@gmail.com>
2020-04-23 17:17:58 -0400

    Updates

---
+++ precache-manifest.d8efd789ef36dd2e745ce705f5a68219.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78755a934271d713ba434f8cab3165b",
+    "url": "/Portfolio-MarciaAdler/index.html"
+  },
+  {
+    "revision": "f7c6f1711f8ce92430bd",
+    "url": "/Portfolio-MarciaAdler/static/css/main.f425a06e.chunk.css"
+  },
+  {
+    "revision": "741b1ce9ab0751a86f93",
+    "url": "/Portfolio-MarciaAdler/static/js/2.56da070f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Portfolio-MarciaAdler/static/js/2.56da070f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c6f1711f8ce92430bd",
+    "url": "/Portfolio-MarciaAdler/static/js/main.cf12e135.chunk.js"
+  },
+  {
+    "revision": "9b090edf2077c12fe447",
+    "url": "/Portfolio-MarciaAdler/static/js/runtime-main.b063c9a1.js"
+  },
+  {
+    "revision": "01980763ec93db5255d87cbd9266f19d",
+    "url": "/Portfolio-MarciaAdler/static/media/backgroundimage.01980763.jpg"
+  },
+  {
+    "revision": "16d51c9bf9ac3836bdc77948d27fdc5e",
+    "url": "/Portfolio-MarciaAdler/static/media/me.16d51c9b.jpeg"
+  }
+]);
