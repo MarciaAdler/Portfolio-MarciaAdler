c3bedc2604106bbeeef198141d46cf7da6f8b31d
Marcia <marciaadler718@gmail.com>
2020-04-23 18:32:40 -0400

    Updates

--- precache-manifest.7debae55499c26f8264c5d59a72da7dd.js
+++ precache-manifest.7debae55499c26f8264c5d59a72da7dd.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42d180dc85923a85bb8b77fd73454502",
+    "revision": "5370e14f3d0a6b61af7e99559e0cabf3",
     "url": "/Portfolio-MarciaAdler/index.html"
   },
   {
-    "revision": "e80b7a24e6f94869bc40",
+    "revision": "0668160d07937dbcd5d6",
     "url": "/Portfolio-MarciaAdler/static/css/main.40c01e72.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio-MarciaAdler/static/js/2.56da070f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e80b7a24e6f94869bc40",
-    "url": "/Portfolio-MarciaAdler/static/js/main.e58da905.chunk.js"
+    "revision": "0668160d07937dbcd5d6",
+    "url": "/Portfolio-MarciaAdler/static/js/main.ad1e48e4.chunk.js"
   },
   {
     "revision": "9b090edf2077c12fe447",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-MarciaAdler/precache-manifest.06e38dde5671720e7d10efa283cc5678.js"
+  "/Portfolio-MarciaAdler/precache-manifest.7debae55499c26f8264c5d59a72da7dd.js"
 );
 
 self.addEventListener('message', (event) => {
